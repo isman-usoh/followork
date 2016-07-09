8763cf6c7fe8627e894a8438832f27ddece5fa18
Isman Usoh <isman.usoh@gmail.com>
2016-07-09 17:39:59 +0700

    create team page

---
+++ app/pages/team/team.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  templateUrl: "build/pages/team/team.html",
+})
+export class TeamPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
