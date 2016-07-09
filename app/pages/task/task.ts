89a93a7679e97150162a5befe255a946ad9d965c
Isman Usoh <isman.usoh@gmail.com>
2016-07-09 17:41:49 +0700

    reformat and fix head for ionic beta 10

--- app/pages/task/task.ts
+++ app/pages/task/task.ts
@@ -1,15 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
-/*
-  Generated class for the TaskPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  templateUrl: 'build/pages/task/task.html',
+    templateUrl: "build/pages/task/task.html",
 })
 export class TaskPage {
-  constructor(private nav: NavController) {}
+    constructor(private nav: NavController) { }
 }
