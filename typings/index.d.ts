b74fbe9f8cc8e23569f88cab0ed20ccfaee4da69
Isman Usoh <isman.usoh@gmail.com>
2016-08-24 14:04:28 +0700

    add node typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/faker/index.d.ts" />
 /// <reference path="globals/firebase3/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
